df1db5e7851c7deab6ea3d4d008ac3156cedb5dc
aaaaaaaaaaaaaaaaace <47800342+aaaaaaaaaaaaaaaaace@users.noreply.github.com>
2019-03-09 14:30:05 +0800

    Update q06_kilograms_to_pounds.py

--- p02/q06_kilograms_to_pounds.py
+++ p02/q06_kilograms_to_pounds.py
@@ -1,10 +1,10 @@
-def kilograms_to_pounds(max_kilo):
-    kilograms = 0
-    print("kilograms" + "\t" + "pounds")
-    for kilograms in range(max_kilo):
-        kilograms = kilograms + 1
-        pounds = kilograms * 2.2
-        print(kilograms , "\t\t" , "{0:.1f}".format(pounds))
+def miles_to_kilometers(max_kilo):
+    miles = 0
+    print("miles" + "\t\t" + "kilometers", "\t", 'kilometers', '\t\t', 'miles')
+    for miles in range(max_kilo):
+        miles = miles + 1
+        kilometers = miles * 1.609
+        print(miles , "\t\t" , "{0:.3f}".format(kilometers), "\t\t", (miles * 5 + 15), '\t\t', "{0:.3f}".format(((kilometers * 5 + 15) /1.609)))
+        
 
-
-kilograms_to_pounds(10)
+miles_to_kilometers(10)
