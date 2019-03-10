889a18e8f4fdd7cd2c2de72b336783b14d21b239
aaaaaaaaaaaaaaaaace <47800342+aaaaaaaaaaaaaaaaace@users.noreply.github.com>
2019-03-10 21:38:01 +0800

    Update q12_find_factors.py

--- p02/q12_find_factors.py
+++ p02/q12_find_factors.py
@@ -1,1 +1,26 @@
-#wait
+number = 120
+factor = 2
+while number % factor == 0:
+    number = number / factor
+    print(factor)
+    if number % factor != 0:
+        factor = factor + 1
+        while number % factor == 0:
+            number = number / factor
+            print(factor)
+            if number % factor != 0:
+                factor = factor + 2
+                while number % factor == 0:
+                    number = number / factor
+                    print(factor)
+                    if number % factor != 0:
+                        factor = factor + 2
+                        while number % factor == 0:
+                            number = number / factor
+                            print(factor)
+                        if factor > number:
+                            break
+                        elif factor == number:
+                            print(factor)
+                        else:
+                            break
