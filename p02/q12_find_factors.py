905bffab771a99331d3766a413e15cb141c44883
aaaaaaaaaaaaaaaaace <47800342+aaaaaaaaaaaaaaaaace@users.noreply.github.com>
2019-03-11 21:26:20 +0800

    Update q12_find_factors.py

--- p02/q12_find_factors.py
+++ p02/q12_find_factors.py
@@ -1,26 +1,29 @@
-number = 120
-factor = 2
-while number % factor == 0:
-    number = number / factor
-    print(factor)
-    if number % factor != 0:
-        factor = factor + 1
-        while number % factor == 0:
-            number = number / factor
-            print(factor)
-            if number % factor != 0:
-                factor = factor + 2
-                while number % factor == 0:
-                    number = number / factor
-                    print(factor)
-                    if number % factor != 0:
-                        factor = factor + 2
-                        while number % factor == 0:
-                            number = number / factor
-                            print(factor)
-                        if factor > number:
-                            break
-                        elif factor == number:
-                            print(factor)
-                        else:
-                            break
+ def getNextFactor(factor):
+    factor=factor+1
+    while True:
+        for i in range(2,factor):
+            if factor % i == 0:
+                #print ( factor,"is divisble by",i)
+                factor = factor+1
+                break
+        else:
+            #print (factor, "is a prime")
+            break
+    return factor        
+
+
+def getAllFactors(number):
+    factor = 2
+    while True:
+
+              if (number % factor == 0):
+                print(factor)
+                number = number / factor
+              else:
+                factor=getNextFactor(factor)
+
+              if factor > number:
+                break
+            
+number = int(input('Enter number:'))
+getAllFactors(number) 
