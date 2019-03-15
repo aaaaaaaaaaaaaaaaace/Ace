f2cfd22bc663298ca4d96c2b7f0014728f80f57e
aaaaaaaaaaaaaaaaace <47800342+aaaaaaaaaaaaaaaaace@users.noreply.github.com>
2019-03-15 23:05:27 +0800

    Update q3_find_gcd.py

--- p03/q3_find_gcd.py
+++ p03/q3_find_gcd.py
@@ -1,9 +1,23 @@
 m = int(input('Enter bigger integer: '))
 n = int(input('Enter smaller integer: '))
 factor = n-1
-if m % n == 0:
-    print(n)
 
 while True:
-    if m % factor != 0 and n % factor != 0:
-        factor = factor - 1 # what i should be aiming for is when the 'if' thing becomes false, print factor. continue later.
+    a = m % factor
+    b = n % factor
+    
+    if m < n:
+        print('Enter the larger integer first')
+        break
+    
+    elif a or b != 0:
+        factor = factor - 1
+        
+    elif m % n == 0:
+        print(n)
+        break
+
+    elif a == 0 and b == 0:
+        print(factor)
+        break
+        
